fa8f19ed031f7b0c88b3a6aaa0a3ef299a9ff8b0
Swapnil-09 <swapnil09.sj@gmail.com>
2024-04-11 13:00:06 +0530

    log and exception files

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 from src.covid19_classification.logger import logging
+from src.covid19_classification.exception import CustomException
 
+import sys
 
 if __name__=="__main__":
     logging.info("The execution has started")
+
+
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Custom Exception")
+        raise CustomException(e,sys)
