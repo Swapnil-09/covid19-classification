01cccd2296da982d4794bb09bf8b4915971d6539
Swapnil-09 <swapnil09.sj@gmail.com>
2024-04-11 19:03:38 +0530

    Data ingested

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from src.covid19_classification.logger import logging
 from src.covid19_classification.exception import CustomException
-
+from src.covid19_classification.components.data_ingestion import DataIngestion
 import sys
 
 if __name__=="__main__":
@@ -8,7 +8,9 @@ if __name__=="__main__":
 
 
     try:
-        a=1/0
+        data_ingestion=DataIngestion()
+        train_data_path, test_data_path=data_ingestion.initiate_data_ingestion()
+
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e,sys)
